15fc30af9d837f0ae2805ea6a2d730e09255ad5b
cgnerds <cgnerds@gmail.com>
2017-07-11 20:58:57 +0800

    Refactoring the permission structure and simplify the OpenCV related functions.

--- app/src/main/java/com/magic_ar/slamar_android/MainActivity.java
+++ app/src/main/java/com/magic_ar/slamar_android/MainActivity.java
@@ -2,59 +2,36 @@ package com.magic_ar.slamar_android;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.ContentValues;
-import android.content.Intent;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
-import android.net.Uri;
 import android.os.Build;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Toast;
+import android.view.SurfaceView;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
 
-import java.io.File;
+import java.util.HashMap;
 import java.util.List;
 
-import permissions.dispatcher.NeedsPermission;
-import permissions.dispatcher.RuntimePermissions;
-
-@RuntimePermissions
+@SuppressWarnings("deprecation")
 public class MainActivity extends AppCompatActivity
         implements CameraBridgeViewBase.CvCameraViewListener2 {
+
     // A tag for log output.
     private static final String TAG = MainActivity.class.getSimpleName();
-    // A key for storing the index of the active camera.
-    private static final String STATE_CAMERA_INDEX = "cameraIndex";
-    // A key for storing the index of the active iamge size.
-    private static final String STATE_IMAGE_SIZSE_INDEX = "imageSizeIndex";
-    // An ID for items in the image size submenu.
-    private static final int MENU_GROUP_ID_SIZE = 2;
-    // The index of the active camera.
-    private int mCameraIndex;
-    // The index of the active image size.
-    private int mImageSizeIndex;
     // The camera view.
     private CameraBridgeViewBase mCameraView;
-    // The image sizes supported by the active camera.
-    private List<Camera.Size> mSupportedImageSizes;
-
     // The OpenCV loader callback
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -70,7 +47,6 @@ public class MainActivity extends AppCompatActivity
                     super.onManagerConnected(status);
                     break;
             }
-            super.onManagerConnected(status);
         }
     };
 
@@ -78,45 +54,64 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Load the OpenCV package.
+        // Load the OpenCV package first.
         OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
 
-        if(savedInstanceState != null) {
-            mCameraIndex = savedInstanceState.getInt(
-                    STATE_CAMERA_INDEX, 0);
-            mImageSizeIndex = savedInstanceState.getInt(
-                    STATE_IMAGE_SIZSE_INDEX, 0);
-        } else {
-            mCameraIndex = 0;
-            mImageSizeIndex = 0;
-        }
+        requestCameraPermission(new PermissionCallback() {
+            @Override
+            public void onSuccess() {
+                setContentView(R.layout.activity_main);
+                mCameraView = (CameraBridgeViewBase) findViewById(R.id.openCvView);
+                mCameraView.setVisibility(SurfaceView.VISIBLE);
+                mCameraView.setCvCameraViewListener(MainActivity.this);
+            }
 
-        setContentView(R.layout.activity_main);
-        // Request for camera permission
-        MainActivityPermissionsDispatcher.setupCameraWithCheck(this);
+            @Override
+            public void onFailure() {
+            }
+        });
     }
 
-    @SuppressLint("NewApi")
-    @NeedsPermission(Manifest.permission.CAMERA)
-    public void setupCamera() {
-        final Camera camera;
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
-            Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
-            Camera.getCameraInfo(mCameraIndex, cameraInfo);
-            camera = Camera.open(mCameraIndex);
+    private interface PermissionCallback {
+        void onSuccess();
+        void onFailure();
+    }
+    private HashMap<Integer, PermissionCallback> permissionCallbacks = new HashMap<Integer, PermissionCallback>();
+    private int permissionRequestCodeSerial = 0;
+    @TargetApi(23)
+    public void requestCameraPermission(PermissionCallback callback) {
+        if(Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                int requestCode = permissionRequestCodeSerial;
+                permissionRequestCodeSerial += 1;
+                permissionCallbacks.put(requestCode, callback);
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, requestCode);
+            } else {
+                callback.onSuccess();
+            }
         } else {
-            // Assume there is only 1 camera and it is rear-facing
-            camera = Camera.open();
+            callback.onSuccess();
         }
+    }
 
-        final Camera.Parameters parameters = camera.getParameters();
-        camera.release();
-        mSupportedImageSizes = parameters.getSupportedPreviewSizes();
-        final Camera.Size size = mSupportedImageSizes.get(mImageSizeIndex);
-        mCameraView = new JavaCameraView(this, mCameraIndex);
-        mCameraView.setMaxFrameSize(size.width, size.height);
-        mCameraView.setCvCameraViewListener(this);
-        setContentView(mCameraView);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        if (permissionCallbacks.containsKey(requestCode)) {
+            PermissionCallback callback = permissionCallbacks.get(requestCode);
+            permissionCallbacks.remove(requestCode);
+            boolean executed = false;
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    executed = true;
+                    callback.onFailure();
+                }
+            }
+            if (!executed) {
+                callback.onSuccess();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
@@ -139,13 +134,6 @@ public class MainActivity extends AppCompatActivity
             mCameraView.disableView();
         }
         super.onDestroy();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        // NOTE: delegate the permission handling to generated method
-        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
     }
 
     @Override
@@ -173,7 +161,6 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        final Mat rgba = inputFrame.rgba();
-        return rgba;
+        return inputFrame.rgba();
     }
 }
