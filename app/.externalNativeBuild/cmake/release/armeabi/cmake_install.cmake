f7b588a3f253a33a92f13b5eb8d307243cc4894d
cgnerds <cgnerds@gmail.com>
2017-07-11 14:48:22 +0800

    Delete the .externalNativeBuild\cmake file.

--- app/.externalNativeBuild/cmake/release/armeabi/cmake_install.cmake
+++ app/.externalNativeBuild/cmake/release/armeabi/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Source/AndroidTest/AndroidCV/SlamARAndroid/app
+# Install script for directory: D:/Source/SlamARAndroid/app
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Source/AndroidTest/AndroidCV/SlamARAndroid/app/.externalNativeBuild/cmake/release/armeabi/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Source/SlamARAndroid/app/.externalNativeBuild/cmake/release/armeabi/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
