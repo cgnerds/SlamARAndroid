6ca1905c72d340687f12fce56aedf6713c1f42cf
cgnerds <cgnerds@gmail.com>
2017-08-09 22:42:30 +0800

    Two new class initialization.

---
+++ MagicAR/src/main/java/com/magicar/MagicGLRendererBase.java
@@ -0,0 +1,15 @@
+package com.magicar;
+
+import android.graphics.SurfaceTexture;
+import android.opengl.GLSurfaceView;
+import android.view.SurfaceView;
+
+/**
+ * Created by 38325 on 2017/8/9.
+ */
+
+public abstract class MagicGLRendererBase implements GLSurfaceView.Renderer, SurfaceTexture.OnFrameAvailableListener{
+    protected final String TAG = "MagicGLRendererBase";
+
+    protected SurfaceTexture mSTex;
+}
---
+++ MagicAR/src/main/java/com/magicar/MagicGLSurfaceView.java
@@ -0,0 +1,24 @@
+package com.magicar;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.os.Build;
+
+import org.opencv.android.Camera2Renderer;
+import org.opencv.android.CameraGLSurfaceView;
+
+/**
+ * Created by 38325 on 2017/8/9.
+ */
+
+public class MagicGLSurfaceView extends GLSurfaceView {
+    MagicGLRendererBase mRenderer;
+
+    public MagicGLSurfaceView(Context context) {
+        super(context);
+
+        if(Build.VERSION.SDK_INT >= 21) {
+            // mRenderer = new Camera2Renderer(this);
+        }
+    }
+}
