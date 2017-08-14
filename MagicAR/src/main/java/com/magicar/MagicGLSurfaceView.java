fae6b7878f89a5612eda770b1cadd0547f7fbba0
cgnerds <cgnerds@gmail.com>
2017-08-14 22:11:30 +0800

    Update MagicGLSurfaceView.java

--- MagicAR/src/main/java/com/magicar/MagicGLSurfaceView.java
+++ MagicAR/src/main/java/com/magicar/MagicGLSurfaceView.java
@@ -19,6 +19,7 @@ public class MagicGLSurfaceView extends GLSurfaceView {
 
         if(Build.VERSION.SDK_INT >= 21) {
             // mRenderer = new Camera2Renderer(this);
+            // mRenderer = new Camera2Renderer(this);
 
         }
     }
